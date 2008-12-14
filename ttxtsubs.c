5c92f69af85b8927b864b38b2b59bbc52132547f
Tobias Grimm <tobias@e-tobi.loc>
2008-12-14 21:32:12 +0100

    Fixed wrong GPL license text (wrong FSF address), clarified license (it's GPL-2 or any later) and added a licence  header to every source file

--- ttxtsubs.c
+++ ttxtsubs.c
@@ -1,9 +1,21 @@
 /*
- * ttxtsubs.c: A plugin for the Video Disk Recorder
+ * vdr-ttxtsubs - A plugin for the Linux Video Disk Recorder
+ * Copyright (c) 2003 - 2008 Ragnar Sundblad <ragge@nada.kth.se>
  *
- * See the README file for copyright information and how to reach the author.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
  *
- * $Id: ttxtsubs.c,v 1.26 2004/05/31 19:59:14 ragge Exp $
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
  */
 
 #include <vdr/plugin.h>
